b7c047edac316d4e9443e9108f02809068498aa0
amd88 <amodadissanayake@gmail.com>
2017-09-16 19:36:54 +0530

    category completed

--- category.php
+++ category.php
@@ -39,5 +39,33 @@ function mapInvoice($BookName,$CategoryName){
 	
 	
 	
+	$customer=$client->makeNode();
+	$customer->setProperty('CategoryName',$CategoryName)
+		
+		->save();
+		$Label = $client->makeLabel('Category');
+		$labels = $customer->addLabels(array($Label));	
+	
+	$CheckIndex = new Everyman\Neo4j\Index\NodeIndex($client, 'customer');
+	$match=$CheckIndex->findOne('CategoryName',$CategoryName);
+	//echo "dddd";
+//	echo $match;
+	if($match){
+//		echo "oooooo";
+		$customerID = $customer->getId();
+		$wrong = $client->getNode($customerID);
+		$wrong->delete();
+		$match->relateTo($invoice, 'PURCHASE')
+				 ->save();
+	}
+	else{
+				 
+	$Index = new Everyman\Neo4j\Index\NodeIndex($client, 'customer');
+	$Index->save();
+//	echo $customer->getProperty('CustomerCode');
+	$Index->add($customer, 'CustomerCode', $customer->getProperty('CategoryName'));
 
+	$customer->relateTo($invoice, 'BELONG')
+				 ->save();
+	}
 }}
